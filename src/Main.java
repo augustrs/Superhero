839692ead4438abf5845b4670f8da2a4a715e6ac
augustrs <auso0001@stud.kea.dk>
2023-09-11 15:19:03 +0200

    added brugerinput og sout

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Indtast navnet på din superhelt: ");
+        String name = scanner.nextLine();
+        System.out.println("Indtast superkræften på din superhelt: ");
+        String superPower = scanner.nextLine();
+        System.out.println("Indtast superheltens creation year: ");
+        int oprettelsesÅr = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Indtast superheltens styrke: ");
+        String styrke = scanner.nextLine();
+
+
+        System.out.println(name);
+        System.out.println(superPower);
+        System.out.println(oprettelsesÅr);
+        System.out.println(styrke);
+    }
 }
