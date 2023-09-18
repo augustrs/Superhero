0b76851cda7372b5d9dac851e2b2970873e5a516
Joakim224 <joakimbusiness224@gmail.com>
2023-09-18 12:31:30 +0200

    Changes by augustreinholdsorensen

--- src/Main.java
+++ src/Main.java
@@ -14,33 +14,7 @@ public class Main {
 
             if (menuValg == 1) {
 
-                System.out.println("Indtast navnet på din superhelt: ");
-                String navn = scanner.nextLine();
-                System.out.println("Indtast din superhelts rigtige navn: ");
-                String ægteNavn = scanner.nextLine();
-                System.out.println("Indtast superkræften på din superhelt: ");
-                String superKræft = scanner.nextLine();
-                System.out.println("Indtast superheltens creation year: ");
-                while (!scanner.hasNextInt()) {
-                    System.out.println("Du skal indtaste et tal: ");
-                    scanner.nextLine();
-                }
-                int oprettelsesÅr = scanner.nextInt();
 
-                scanner.nextLine();
-                System.out.println("Indtast superheltens styrke: ");
-                String styrke = scanner.nextLine();
-                System.out.println("Er superhelten menneske? (j/n): ");
-                char menneske = scanner.next().charAt(0);
-                boolean erMenneske = false;
-                if (menneske == 'j') {
-                    erMenneske = true;
-                }
-                if (menneske == 'n') {
-                    erMenneske = false;
-                }
-
-                database.addSuperhero(navn, ægteNavn, superKræft, oprettelsesÅr, erMenneske, styrke);
 
             }
             else if (menuValg == 3) {
